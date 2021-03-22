12e0163e3830811250bd31bc3085ce2f33c1362c
lattila1 <73441214+lattila1@users.noreply.github.com>
2021-03-22 13:08:48 +0100

    Show details

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 export default function Hotel({ hotel }) {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
-    <div>
-      <h2>{hotel.name}</h2>
-    </div>
+    <>
+      <h2>
+        {hotel.name}{" "}
+        <button
+          onClick={() => {
+            setShowDetails((prevState) => !prevState);
+          }}
+        >
+          Show {showDetails ? "less" : "more"}
+        </button>
+      </h2>
+      {showDetails && (
+        <div>
+          {hotel.city} ({hotel.stars})
+        </div>
+      )}
+    </>
   );
 }
